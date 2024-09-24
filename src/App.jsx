97842e3a8191750b7de32544bb5e297b2b2b9bab
suraj singh rajput <surajthakurrs45@gmail.com>
2024-09-24 10:40:57 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import  Images  from './constants/Images'
+
+function App() {
+    return (
+    <>
+   <img src={Images.logo} alt={Images.logo} />
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/Images.js
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.png';
+
+
+
+
+
+export default {
+    logo
+}
+
